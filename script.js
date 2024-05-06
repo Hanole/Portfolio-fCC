22c73d89e94be5088343311581f964338fcf89ed
Hanole <Olemagnus_Stallvik@hotmail.no>
2024-05-06 23:28:55 +0200

    some big changes

---
+++ script.js
@@ -0,0 +1,22 @@
+const navbar = document.querySelector('#Navbar');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
+
+	setSliderCss() {
+		let width = 0;
+		let left = 0;
+		if(this.currentTab) {
+			width = this.currentTab.css('width');
+			left = this.currentTab.offset().left;
+		}
+		$('.nav-slider').css('width', width);
+		$('.nav-slider').css('left', left);
+	}
+	
